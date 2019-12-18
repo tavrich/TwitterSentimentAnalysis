593bb4f12ad805c282f8a696a43b421ccef64835
tavrich <58997763+tavrich@users.noreply.github.com>
2019-12-18 02:19:22 -0500

    added comments and cleaned up code

--- setiment.R
+++ setiment.R
@@ -1,3 +1,15 @@
+##FINAL PROJECT-Programming for Analytics-Richard Taveras##
+#Install the following packages
+install.packages("twitteR")
+install.packages("RCurl")
+install.packages("httr")
+install.packages("tm")
+install.packages("wordcloud")
+install.packages("syuzhet")
+
+
+
+
 library(twitteR)
 library(RCurl)
 library(httr)
@@ -35,23 +47,32 @@ tweets.df = gsub("[[:punct:]]", "", tweets.df)
 tweets.df = gsub("[[:digit:]]", "", tweets.df)
 #remove links
 tweets.df = gsub("http\\w+", "", tweets.df)
+#remove tabs
 tweets.df = gsub("[ \t]{2,}", "", tweets.df)
+#remove white space and extra spaces
 tweets.df = gsub("^\\s+|\\s+$", "", tweets.df)
+#remove the word trump
 tweets.df = gsub("\\Trump\\b", "",tweets.df)
-
+#set a sentiment variable and get the sentiment from the tweets
 sentiment <- get_nrc_sentiment(tweets.df)
+#sum up each sentiment
 sentBar = colSums(sentiment)
+#label the data
 sentSum = data.frame(count=sentBar, sentiment=names(sentBar))
+#
 sentSum$sentiment = factor(sentSum$sentiment, levels=sentSum$sentiment[order(sentSum$count, decreasing = TRUE)])
 
-# Visualize the sentiments from NRC sentiments
+# use plotly to create the chart
 library(plotly)
 p <- plot_ly(sentSum, x=~sentiment, y=~count, type="bar", color=~sentiment) %>%
+  #label both axis
   layout(xaxis=list(title=""), showlegend=FALSE,
+    #give cloud a title
          title="Sentiment on Impeachment Eve")
+#view the chart
 p
 
-# Create comparison word cloud data
+#set up the data for the wordcloud
 
 wordcloud_tweet = c(
   paste(tweets.df[sentiment$anger > 0], collapse=" "),
@@ -64,27 +85,28 @@ wordcloud_tweet = c(
   paste(tweets.df[sentiment$trust > 0], collapse=" ")
 )
 
-# create corpus
+# create a corpus from the wordcloud data
 tweetCorpus = Corpus(VectorSource(wordcloud_tweet))
-
-# remove punctuation, convert every word in lower case and remove stop words
-
+#remove puncuation
 tweetCorpus = tm_map(tweetCorpus, removePunctuation)
+#remove stopwords 
 tweetcorpus = tm_map(tweetCorpus, removeWords, c(stopwords("english")))
+#keep only stems of words
 corpus = tm_map(tweetCorpus, stemDocument)
 
-# create document term matrix
-
+# create a term document matrix from the corpus
 tdmatrix = TermDocumentMatrix(corpus)
 
-# convert as matrix
+# convert it to a matrix
 tdmatrix = as.matrix(tdmatrix)
 tdmatrixnew <- tdmatrix[nchar(rownames(tdmatrix)) < 11,]
 
-# column name binding
+#bind the column names
 colnames(tdmatrix) = c('anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust')
 colnames(tdmatrixnew) <- colnames(tdmatrix)
+#set the colors and other parameters of the cloud
 comparison.cloud(tdmatrixnew, random.order=FALSE,
                  colors = c("#00B2FF", "red", "#FF0099", "#6600CC", "green", "orange", "blue", "brown"),
                  title.size=1, max.words=250, scale=c(2.5, 0.4),rot.per=0.4)
 
+STOP="STOP"
